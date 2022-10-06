01e4473f1381217788dc03255997be0eff286e6a
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2022-10-06 14:21:50 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2>My Luxury Motel</h2>
+    <div>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
       </Routes>
     </div>
   );
--- src/__steps.js
+++ src/__steps.js
@@ -1,3 +1,5 @@
+// firebase setup steps;
+
 /*
 1.Create a project into firebase;
 2.Register App;
@@ -8,7 +10,9 @@
 
 
 */
+
 // react-router setup steps;
+
 /*
 1.yarn add react-router-dom@6(install into cmd);
 2.import {BrowserRouter}from 'react-router-dom' and wrap app with it;
@@ -16,7 +20,9 @@
 4.
 
  */
+
 // Activites in hole project;
+
 /*
 1.Create a pages folder into it create Home folder and into it create Home component(like Home.js & Home.css);
 2.Create a Shared folder into it create Header folder and into it create Header component(like Header.js & Header.css);
@@ -24,7 +30,16 @@
 
  */
 
+// Framework using steps(like react-bootstrap);
+
+/*
+1.npm install react-bootstrap bootstrap@5.1.3 and import 'bootstrap/dist/css/bootstrap.min.css' into index.js;
+2.
+ */
+
+
 // firebase hosting steps;
+
 /*
 1.npm install -g firebase-tools;
 2.firebase login;
@@ -32,3 +47,4 @@
 4.firebase build;
 5.firebase deploy;
  */
+// Following above steps you can nicely complete a website(like educational,ecommerce,hotel,motel etc);
